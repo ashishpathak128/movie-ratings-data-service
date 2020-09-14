d11a3906cce48057b2a795f856cc37c67610c01b
ashishpathak128 <pathakashish128@yahoo.com>
2020-09-14 18:22:21 +0530

    Commit 2

---
+++ src/main/java/io/ashishworks/movieratingsResource/RatingsDataResource.java
@@ -0,0 +1,16 @@
+package io.ashishworks.movieratingsResource;
+
+import io.ashishworks.movieratingsResource.Models.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratings")
+public class RatingsDataResource {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId){
+        return new Rating(movieId,4);
+    }
+}
