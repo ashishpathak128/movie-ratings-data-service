b1e9a36d4b58d33f5ab51c380c072dcc1e5c8bd7
ashishpathak128 <pathakashish128@yahoo.com>
2020-09-23 17:38:40 +0530

    Final with Eureka client

--- src/main/java/io/ashishworks/movieratingsdataservice/MovieRatingsDataServiceApplication.java
+++ src/main/java/io/ashishworks/movieratingsdataservice/MovieRatingsDataServiceApplication.java
@@ -2,8 +2,10 @@ package io.ashishworks.movieratingsdataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieRatingsDataServiceApplication {
 
 	public static void main(String[] args) {
